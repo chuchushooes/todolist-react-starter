8653baffb96eaf79d08df767422770575cece930
chuchushooes <chuchushooes@gmail.com>
2022-12-29 18:50:17 +0800

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="singup" element={<SignUpPage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+/* 路由元件屬性意義
+BrowserRouter => 讓其底下所有的component都能使用router
+Routes => 用來定義哪個路徑配對哪個元件
+path => 屬性對應網址
+element => 屬性對應元件名稱
+path="*" 是首頁的意思
+*/
